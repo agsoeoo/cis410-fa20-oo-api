74740b660b442f9c2039ffa211d23f8a9b29eaa9
oo <aungoosoe@gmail.com>
2020-10-27 15:04:44 -0600

    10.1 10/27 initial comit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express();
+
+
+app.get("/hi",(req,res)=>{
+    res.send("Hello world")
+
+})
+
+
+
+app.listen(5000,()=>{
+    console.log("App is running on port 5000")
+})
+
