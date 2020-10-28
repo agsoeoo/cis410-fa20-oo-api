7bf3bce40dffef4aeee5d5c3075ae8878db3fbe5
oo <aungoosoe@gmail.com>
2020-10-27 18:08:03 -0600

    10.1 10/27 /movies/:pk

--- index.js
+++ index.js
@@ -24,6 +24,35 @@ app.get("/movies",(req,res)=>{
     
 })
 
+
+app.get("/movies/:pk", (req,res)=>{
+    var pk = req.params.pk
+    console.log("my PK:", pk)
+
+     var myQuery = `select * 
+     from Movie 
+     left join Genre
+     ON Genre.GenrePK = Movie.MoviePK
+     WHERE moviePK = ${pk}`
+
+     db.executeQuery(myQuery)
+     .then((movies)=>{
+        //  console.log("Movies: ", movies)
+        if(movies[0]){
+            res.send(movies[0])
+        }
+        else{
+            res.status(404).send('bad request')
+        }
+     })
+     .catch((err)=>
+     {
+         console.log('Error in /movies/pk',err)
+         res.status(500).send()
+     })
+})
+
+
 app.listen(5000,()=>{
     console.log("App is running on port 5000")
 })
